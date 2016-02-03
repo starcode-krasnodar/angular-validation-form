966a780f7851bae94efb0bdab67917aaf93d8f40
Memory Clutter <memclutter@gmail.com>
2016-02-03 10:29:25 +0300

    Angular code style

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 var app = angular.module("validationFormExample", []);
 
 app.controller("mainController", function($scope) {
+
+    var vm = this;
+
     // модели
-    $scope.driverLicense = null;
-    $scope.registerNumber = null;
+    vm.driverLicense = null;
+    vm.registerNumber = null;
 
     // ошибки валидации
-    $scope.driverLicenseErrors = [];
-    $scope.registerNumberErrors = [];
+    vm.driverLicenseErrors = [];
+    vm.registerNumberErrors = [];
 
     /**
      * Вызывается при изменении значения поля водительских прав.
      *
      * @param driverLicense
      */
-    $scope.driverLicenseChange = function(driverLicense) {
+    vm.driverLicenseChange = function(driverLicense) {
         if (driverLicense.substr(2, 2) != '' && driverLicense.substr(2, 2).match(/[\d+О]/i)) {
-            $scope.driverLicense = driverLicense.replace(/О/i, 0);
+            vm.driverLicense = driverLicense.replace(/О/i, 0);
         }
     };
 
@@ -25,20 +28,20 @@ app.controller("mainController", function($scope) {
      *
      * @param driverLicense string
      */
-    $scope.driverLicenseValidate = function(driverLicense) {
-        $scope.driverLicenseErrors = [];
+    vm.driverLicenseValidate = function(driverLicense) {
+        vm.driverLicenseErrors = [];
 
         if (driverLicense == null || driverLicense == '') {
-            $scope.driverLicenseErrors.push('Поле не может быть пустым');
+            vm.driverLicenseErrors.push('Поле не может быть пустым');
         } else {
-            if (driverLicense.substr(0, 2) != '' && !driverLicense.substr(0, 2).match(/^\d{2}$/)) {
-                $scope.driverLicenseErrors.push('Первые два символа, должны быть цифрами');
+            if (driverLicense.substr(0, 2) == '' || !driverLicense.substr(0, 2).match(/^\d{2}$/)) {
+                vm.driverLicenseErrors.push('Первые два символа, должны быть цифрами');
             }
-            if (driverLicense.substr(2, 2) != '' && !driverLicense.substr(2, 2).match(/^(\d{2}|[А-Я]{2})$/)) {
-                $scope.driverLicenseErrors.push('На месте ХХ могут быть цифры или русские буквы в верхнем регистре');
+            if (driverLicense.substr(2, 2) == '' || !driverLicense.substr(2, 2).match(/^(\d{2}|[А-Я]{2})$/)) {
+                vm.driverLicenseErrors.push('На месте ХХ могут быть цифры или русские буквы в верхнем регистре');
             }
             if (driverLicense.substr(4) == '' || !driverLicense.substr(4).match(/^\d{6}$/)) {
-                $scope.driverLicenseErrors.push('Последние шесть символов должны быть цифрами');
+                vm.driverLicenseErrors.push('Последние шесть символов должны быть цифрами');
             }
         }
     };
@@ -48,15 +51,26 @@ app.controller("mainController", function($scope) {
      *
      * @param registerNumber string
      */
-    $scope.registerNumberValidate = function(registerNumber) {
-        $scope.registerNumberErrors = [];
+    vm.registerNumberValidate = function(registerNumber) {
+        vm.registerNumberErrors = [];
 
         if (registerNumber == null || registerNumber == '') {
-            $scope.registerNumberErrors.push('Поле не может быть пустым');
+            vm.registerNumberErrors.push('Поле не может быть пустым');
         } else {
             if (!registerNumber.match(/^([А-Я])(\d{3})([А-Я]{2})(\d{2,3})$/)) {
-                $scope.registerNumberErrors.push('Неверный формат, допустим следующий шаблон А 123 АА 12 или А 123 АА 123');
+                vm.registerNumberErrors.push('Неверный формат, допустим следующий шаблон А 123 АА 12 или А 123 АА 123');
             }
+        }
+    };
+
+    /**
+     * Заглушка отправки данных.
+     */
+    vm.submit = function() {
+        if (vm.driverLicenseErrors.length == 0 && vm.registerNumberErrors.length == 0) {
+            console.log('Submit data');
+        } else {
+            console.error('Validation errors');
         }
     };
 });
